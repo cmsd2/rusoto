53539001ea1807b4ed9197895190edc6dec1a715
Chris Dawes <chris.dawes@mendeley.com>
2016-08-31 14:43:28 +0100

    add simple iam test

---
+++ tests/iam.rs
@@ -0,0 +1,22 @@
+#![cfg(feature = "iam")]
+
+extern crate rusoto;
+
+use rusoto::iam::IamClient;
+use rusoto::iam::{GetUserRequest, GetUserError};
+use rusoto::{DefaultCredentialsProvider, Region};
+
+#[test]
+fn main() {
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+
+    let iam = IamClient::new(credentials, Region::UsEast1);
+
+    // http://docs.aws.amazon.com/IAM/latest/APIReference/Welcome.html
+    match iam.get_user(&GetUserRequest{
+            ..Default::default()
+        }) {
+        err =>
+            panic!("error: {}", err)
+    }
+}
