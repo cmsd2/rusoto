1175ea98a030e95af1735f434795d5b8d197fe60
Nikita Pekin <contact@nikitapek.in>
2016-09-05 21:44:49 -0400

    wip: improve IAM get_user test
    Improve IAM `get_user` test.

--- tests/iam.rs
+++ tests/iam.rs
@@ -7,16 +7,14 @@ use rusoto::iam::{GetUserRequest, GetUserError};
 use rusoto::{DefaultCredentialsProvider, Region};
 
 #[test]
-fn main() {
+fn get_user() {
     let credentials = DefaultCredentialsProvider::new().unwrap();
 
     let iam = IamClient::new(credentials, Region::UsEast1);
 
     // http://docs.aws.amazon.com/IAM/latest/APIReference/Welcome.html
-    match iam.get_user(&GetUserRequest{
-            ..Default::default()
-        }) {
-        err =>
-            panic!("error: {:?}", err)
-    }
+    let request = GetUserRequest {
+        ..Default::default()
+    };
+    iam.get_user(&request).unwrap();
 }
